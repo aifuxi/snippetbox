7b7d11f6f6ddca6b726797a7e719c7552cb4135b
aifuxi <aifuxi.js@gmail.com>
2025-03-26 22:53:05 +0800

    feat: init commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aifuxi/snippetbox/internal/models"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+
+	//files := []string{
+	//	"./ui/html/base.tmpl",
+	//	"./ui/html/partials/nav.tmpl",
+	//	"./ui/html/pages/home.tmpl",
+	//}
+	//
+	//parseFiles, err := template.ParseFiles(files...)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
+	//
+	//err = parseFiles.ExecuteTemplate(w, "base", nil)
+	//if err != nil {
+	//	app.serverError(w, err)
+	//	return
+	//}
+}
+
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+			return
+		}
+		app.serverError(w, err)
+		return
+	}
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+
+	parseFiles, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = parseFiles.ExecuteTemplate(w, "base", snippet)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		//w.WriteHeader(http.StatusMethodNotAllowed)
+		//w.Write([]byte("Method Not Allowed"))
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// 重定向
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+}
---
+++ cmd/web/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"github.com/aifuxi/snippetbox/internal/models"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type config struct {
+	addr      string
+	staticDir string
+	dsn       string
+}
+
+type application struct {
+	infoLog  *log.Logger
+	errorLog *log.Logger
+	cfg      config
+	snippets *models.SnippetModel
+}
+
+func main() {
+	var cfg config
+
+	// 定义一个标志 addr
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "staticDir", "./ui/static", "Static files path")
+	flag.StringVar(&cfg.dsn, "dsn", "root:123456@tcp(127.0.0.1:33060)/snippetbox?parseTime=true", "MySQL data source name")
+
+	flag.Parse()
+
+	f, err := os.OpenFile("./logs/info.log", os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	//infoLog := log.New(f, "[INFO]\t", log.Ldate|log.Ltime)
+	infoLog := log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		errorLog.Fatalln(err)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
+		cfg:      cfg,
+		snippets: &models.SnippetModel{
+			DB: db,
+		},
+	}
+
+	infoLog.Printf("Starting server on %v", cfg.addr)
+
+	srv := http.Server{
+		Addr:     cfg.addr,
+		Handler:  app.routes(),
+		ErrorLog: errorLog,
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		errorLog.Fatalln(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
